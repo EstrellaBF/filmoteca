1b27dc0babd18d2db1b7f908fc47581133b24648
EstrellaBF <stern.von.nord>
2018-01-18 11:31:03 -0500

    jalando las imagenes y títulos

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   var $searchForm = $('#search-form');
-  console.log($searchForm);
+  var $moviesSelected = $('#movies-selected')
+  console.log($moviesSelected);
   // 
   $searchForm.on('submit', function(event) {
     var searchText = $searchForm.find('input').val();
@@ -10,37 +11,36 @@ $(document).ready(function() {
     event.preventDefault();
   });
 
-
+// img src=" ' + result.user.photoURL + ' " /
   function getMovies(searchText) {
     console.log(searchText);
     axios.get('http://www.omdbapi.com/?s=' + searchText+'&apikey=3a0eede3')
       .then(function(response) {
-        console.log(response);
+        // Un array con todos los títulos que coinciden
+        console.log(response.data.Search); 
+        var movieSearch = response.data.Search;
+        var output = '';
+        // Recosrrer en un lugar de for
+        $.each(movieSearch, function(index, value) {
+          // console.log(index); // devuelve posición, 0, 1, 2, etc    
+          // console.log(value); //Objeto que contiene tittle, year, etc
+          console.log(movieSearch[index]);
+          output += `
+            <div class="col-xs-6 col-md-3">
+              <div class="movie-box">
+                <img src="${value.Poster}">
+                <h5>${value.Title}</h5>
+              </div>
+            </div>
+          `;
+          // $moviesSelected.append('<div class="col-xs-6"><div class="movie-wrap"><img src=""><h4><a href=""></div></div>');
+          // $('.movie-wrap').find('img').prop('src', movieSearch[index].Poster)
+        });
+        $moviesSelected.html(output);
       })
       .catch(function (err) {
         console.log(err);
       });
   };
 
-  
-  // var $searchMovieInput = $('#search-movie-input');
-  
-
-  // omdb.set_default('apikey', 3a0eede3)
-  // // Capturando el contenido del input
-  // $searchMovieInput.on('input', getTheMovie);
-
-  // // Función para capturar el evento del input
-  // function getTheMovie() {
-  //   $(this).val();
-  // }
-
-  // // Función para llamar al API de OMDB
-  // function apiCall() {
-  //   $.getJSON('https://www.omdbapi.com/?t=' + encodeURI('batman')).then(function(response) {
-  //     console.log(response);
-  //   });
-  // }
-
-  // apiCall();
 });
