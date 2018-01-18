65cb9f223aab704e047729e196b4b97380f3137f
EstrellaBF <stern.von.nord>
2018-01-18 16:56:22 -0500

    arreglando error

--- js/movie.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  var $movieInfoPicked = $('#movie-info-picked');
-
-  // Pluggin
-  $('#star1').rating('votar.php', {maxvalue: 5, curvalue:1, id:20});
-
-  // Cojiendo el valor del título
-  $movieInfoPicked.find('h4').text(localStorage.saveTitlePicked);
-});
