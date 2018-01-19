e6e13123c272702158e9685f68f4c62afbf45eba
lidiecitass <lid_26_90@hotmail.com>
2018-01-18 20:22:45 -0500

    Cambios

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 
 function begin() {
-  
+
   var $searchForm = $('#search-form');
   var $moviesSelected = $('#movies-selected');
   console.log($moviesSelected);
   // 
-  $searchForm.on('submit', function(event) {
+  $searchForm.on('submit', function (event) {
     var searchText = $searchForm.find('input').val();
     // console.log(searchText);
     // Le paso la función getMovies que buscará en el API de OMDB
@@ -16,15 +16,15 @@ function begin() {
   // Función para buscar la película por título
   function getMovies(searchText) {
     console.log(searchText);
-    axios.get('http://www.omdbapi.com/?s=' + searchText +'&apikey=3a0eede3')
-      .then(function(response) {
+    axios.get('http://www.omdbapi.com/?s=' + searchText + '&apikey=3a0eede3')
+      .then(function (response) {
         // Un array con todos los títulos que coinciden
-        console.log(response); 
-        console.log(response.data.Search); 
+        console.log(response);
+        console.log(response.data.Search);
         var movieSearch = response.data.Search;
         var output = '';
         // Recorrer en un lugar de for
-        $.each(movieSearch, function(index, value) {
+        $.each(movieSearch, function (index, value) {
           // console.log(index); // devuelve posición, 0, 1, 2, etc    
           // console.log(value); //Objeto que contiene tittle, year, etc
           // console.log(movieSearch[index]);
@@ -39,30 +39,30 @@ function begin() {
           `;
         });
         $moviesSelected.html(output);
-        
-        
+
+
         // Función para jalar el contenido del botón al hacer click
-        $('.pick-this-movie').on('click', function() {
+        $('.pick-this-movie').on('click', function () {
           var posterMoviePicked = $(this).parent();
           console.log(posterMoviePicked.find('img').attr('src'));
           var titlePicked = $(this).text();
           // Almacenando temporalmente el nombre del título y la foto
           localStorage.saveTitlePicked = titlePicked;
-          $(location).attr('href','views/movie.html');
+          $(location).attr('href', 'views/movie.html');
         });
 
       })
-      .catch(function(err) {
+      .catch(function (err) {
         console.log(err);
       });
   };
-  
+
   /* Función para crear registro de usuario en Firebase*/
   function registry() {
     var validateNames = false;
     var validateEmail = false;
     var validatePassword = false;
-    var validateChecked = false; 
+    var validateChecked = false;
     var $checked = $('input[type="checkbox"]');
 
     /**  Validar todos los campos **/
@@ -76,10 +76,10 @@ function begin() {
     /* Desactivar Boton registrar */
     function desactiveButton() {
       btnRegistry.attr('disabled', true);
-    } 
+    }
 
     /* Validar al escribir en nombres */
-    $('#names').on('input', function() {
+    $('#names').on('input', function () {
       var NAMES = /^([A-z ñáéíóú]{2,8})$/;
       if (NAMES.test($(this).val())) {
         validateNames = true;
@@ -89,26 +89,26 @@ function begin() {
       }
     });
     /* Validar al escribir correo */
-    $('#email').on('input', function() {
+    $('#email').on('input', function () {
       var exp = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/;
       if (exp.test($(this).val())) {
         validateEmail = true;
-        activeButton(); 
+        activeButton();
       } else {
         desactiveButton();
       }
     });
     /* Validar al escribir contraseña */
-    $('#password').on('input', function() {
-      if($(this).val().length > 6) {
+    $('#password').on('input', function () {
+      if ($(this).val().length > 6) {
         validatePassword = true;
-        activeButton(); 
+        activeButton();
       } else {
         desactiveButton();
       }
     });
 
-    $checked.on('click', function(event) {
+    $checked.on('click', function (event) {
       if ($checked.prop('checked')) {
         validateChecked = true;
         activeButton();
@@ -125,13 +125,13 @@ function begin() {
       localStorage.nombres = $('#names').val();
       localStorage.email = $('#email').val();
       localStorage.password = $('#password').val();
-      firebase.auth().createUserWithEmailAndPassword(email, password).then(()=> {
+      firebase.auth().createUserWithEmailAndPassword(email, password).then(() => {
         firebase.database().ref('registrito')
-        .set({
-          nombres: names
-        });
+          .set({
+            nombres: names
+          });
         alert('Registro OK');
-      }).catch(function(error) {
+      }).catch(function (error) {
         /** Handle Errors here. */
         var errorCode = error.code;
         var errorMessage = error.message;
@@ -139,33 +139,33 @@ function begin() {
         alert(errorMessage);
       });
       $('#names').val('');
-        $('#email').val('');
-        $('#password').val('');
-        $checked.prop('checked', false); 
+      $('#email').val('');
+      $('#password').val('');
+      $checked.prop('checked', false);
     });
   }
-  
+
   /* Función para identificar sign in en Firebase.*/
   function login() {
     var validateEmail = false;
     var validatePassword = false;
 
     /* Validar al escribir correo */
-    $('#user').on('input', function() {
+    $('#user').on('input', function () {
       var exp = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/;
       if (exp.test($(this).val())) {
         validateEmail = true;
-        activeButton(); 
+        activeButton();
       } else {
         desactiveButton();
       }
     });
 
     /* Validar al escribir contraseña */
-    $('#key').on('input', function() {
-      if($(this).val().length > 6) {
+    $('#key').on('input', function () {
+      if ($(this).val().length > 6) {
         validatePassword = true;
-        activeButton(); 
+        activeButton();
       } else {
         desactiveButton();
       }
@@ -181,9 +181,9 @@ function begin() {
     /* Desactivar Boton registrar */
     function desactiveButton() {
       btnAccess.attr('disabled', true);
-    } 
+    }
     /* Comparar correo y password */
-    btnAccess.on('click', function(event) {
+    btnAccess.on('click', function (event) {
       event.preventDefault();
       var email2 = $('#user').val();
       var password2 = $('#key').val();
@@ -196,30 +196,30 @@ function begin() {
       alert(errorMessage);
       
     }); */
-    firebase.auth().signInWithEmailAndPassword(email2, password2).then(()=> {
-      $( "span.content-header" ).html( '<h5>'+localStorage.nombres+'</h5>' );
-    }).catch(function(error) {
-      // Handle Errors here.
+      firebase.auth().signInWithEmailAndPassword(email2, password2).then(() => {
+        $("span.content-header").html('<h5>' + localStorage.nombres + '</h5>');
+      }).catch(function (error) {
+        // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
         if (errorCode) {
           alert(errorMessage);
         }
       });
-      
-    /* ------------ FUNCIONALIDAD PARA SUBIR FOTOS ------------*/
-/** Función para postear  */
-function readURL(input) {
-  if (input.files && input.files[0]) {
-    var reader = new FileReader();
-    reader.onload = function(e) {
-      $('#image1')
-        .attr('src', e.target.result);
-    };
-    reader.readAsDataURL(input.files[0]);
-  }
-}
-    });    
+
+      /* ------------ FUNCIONALIDAD PARA SUBIR FOTOS ------------*/
+      /** Función para postear  */
+      function readURL(input) {
+        if (input.files && input.files[0]) {
+          var reader = new FileReader();
+          reader.onload = function (e) {
+            $('#image1')
+              .attr('src', e.target.result);
+          };
+          reader.readAsDataURL(input.files[0]);
+        }
+      }
+    });
   };
 }
 $(document).ready(begin);
--- js/movie.js
+++ js/movie.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
   var $movieInfoPicked = $('#movie-info-picked');
-  var $viewedButton = $('#viewed-button')
+  var $viewedButton = $('#viewed-button');
 
   // Pluggin
-  $('#star1').rating('votar.php', {maxvalue: 5, curvalue:1, id:20});
+  $('#star1').rating('votar.php', { maxvalue: 5, curvalue: 1, id : 20 });
 
   // Cojiendo el valor del título
   $movieInfoPicked.find('h4').text(localStorage.saveTitlePicked);
 
   // Make a request for a user with a given ID
-  axios.get('http://www.omdbapi.com/?s=' + localStorage.saveTitlePicked +'&apikey=3a0eede3')
-    .then(function (response) {
+  axios.get('http://www.omdbapi.com/?s=' + localStorage.saveTitlePicked + '&apikey=3a0eede3')
+    .then(function(response) {
       var movieSearch2 = response.data.Search;
       $.each(movieSearch2, function(index, value) {
         // console.log(index);
@@ -20,7 +20,7 @@ $(document).ready(function() {
           console.log(value.Poster);
           // console.log($movieInfoPicked.find('img').prop('src'));
           $movieInfoPicked.find('img').attr('src', value.Poster)
-        } 
+        }
       });
     })
     .catch(function(error) {
@@ -29,7 +29,7 @@ $(document).ready(function() {
 
   // Evento para el boton de visto
   $viewedButton.on('click', function() {
-    
+
   });
 
 });
